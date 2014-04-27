31f4ce8f3c930cf992d9640f17bd81fda609fbf2
Lex Li <support@lextm.com>
2014-04-27 17:20:36 +0800

    Added assembly info task.

--- lib/SharedAssemblyInfo.cs
+++ lib/SharedAssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Reflection;
 // You can specify all the values or you can use the default the Revision and
 // Build Numbers by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.9.000420.22")]
+[assembly: AssemblyVersion("0.9.000427.00")]
 #if (!CF)
-[assembly: AssemblyFileVersion("0.9.000420.22")]
+[assembly: AssemblyFileVersion("0.9.000427.00")]
 #endif
