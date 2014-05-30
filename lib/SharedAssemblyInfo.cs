190997cadd7760d120c81267225b0cef88cdc9d5
Lex Li <support@lextm.com>
2014-05-30 22:00:39 +0800

    Updated version number.

--- lib/SharedAssemblyInfo.cs
+++ lib/SharedAssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Reflection;
 // You can specify all the values or you can use the default the Revision and
 // Build Numbers by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.9.000526.08")]
+[assembly: AssemblyVersion("1.0.000530.00")]
 #if (!CF)
-[assembly: AssemblyFileVersion("0.9.000526.08")]
+[assembly: AssemblyFileVersion("1.0.000530.00")]
 #endif
